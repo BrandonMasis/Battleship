c0a7fab5009e964e5100518c372836986cf29de1
Brandon Masis <brandmasis@gmail.com>
2023-02-15 18:17:49 -0600

    initial files

---
+++ src/index.test.ts
@@ -0,0 +1,5 @@
+import{sum} from "./index"
+
+test("Sum of 2 & 3 equal 5",()=>{
+    expect((sum(2,3))).toBe(5)
+})
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+// import "./index.scss"
+
+console.log("Hello World!");
+
+
+const sum = (a:number,b:number) => {
+    return a+b
+}
+
+
+export{sum}
